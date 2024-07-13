a181151e90a0fec7f7870b7e879ce26c176134d4
mharisqadri <mharisqadri@gmail.com>
2024-07-13 23:30:46 +0500

    created navbar

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,19 @@
+
+
+function HeroSection() {
+  return (
+    <div 
+    className="h-auto md:h[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
+    >
+    <div className="p-4 relative w-full center">
+<h1>Master your Vocals with Masters</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cupiditate suscipit unde placeat non dignissimos nihil. Maiores, animi alias, nihil iste cumque laborum assumenda debitis omnis minus praesentium saepe nam.</p>
+
+    </div>
+        
+        
+        </div>
+  )
+}
+
+export default HeroSection
